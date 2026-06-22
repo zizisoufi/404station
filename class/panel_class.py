fb606bc7bf4739ba15c3eb888d49be6e04d226cf
Pooya Shourangiz <44345555+pooywa@users.noreply.github.com>
2026-06-22 19:43:55 +0330

    add start class for first view and add admin_login_ to impliment admin methods

--- class/panel_class.py
+++ class/panel_class.py
@@ -7,29 +7,55 @@ class Panel:
             print("\n Be 404 Station Khosh Omadi")
             print("1. Admin Panel")
             print("2. Employer")
-            print("\n Be 404 Station Khosh Omadi")
+            print("3. Passenger")
+            print("4. Exit")
 
-    
-class Start_Panel(Panel):
-    def __init__(self, menu:dict ) :
-        super().__init__(menu)
-        
-class Admin_Panel(Panel):
-    def __init__(self, menu:dict ) :
-        super().__init__(menu)
-        
-        
-class Employer_Panel(Panel):
-    def __init__(self, menu:dict ) :
-        super().__init__(menu)
-          
-    
-class Passenger_Panel(Panel):
-    def __init__(self, menu:dict ) :
-        super().__init__(menu)
-        
+            i = input("Mikhay Koja Beri?")
 
-class Buy_Panel(Panel):
-    def __init__(self, menu:dict ) :
-        super().__init__(menu) 
-    
+            if i == "1":
+                pass
+            elif i == "2":
+                pass
+            elif i == "3":
+                pass
+            elif i == "4":
+                print("Shab O RoozegaR Khosh")
+                break
+            else
+                print("Dari Eshatebah Mizani Dadash")
+        
+        def admin_login_panel(self):
+            while True:
+                print("\n Admin Login")
+
+                username = input("username: ")
+                password = input("password: ")
+
+
+                #pass auth class or db class 
+                
+                #check user and password validation
+
+        def admin_panel(self):
+            while True:
+            print("\nPanel Modiriati")
+            print("1. Add Emplouyer")
+            print("2. Remove Employer")
+            print("3. Show Employers")
+            print("4. Back")
+
+            i = input("Mikhay Koja Beri?")
+
+            if i == "1":
+                pass
+            elif i == "2":
+                pass
+            elif i == "3":
+                pass
+            elif i == "4":
+                return
+            else
+                print("Dari Eshatebah Mizani Dadash")
+
+
+
