0b5196dff9d57ab7f58a208ae394c1c41b007cb9
ali_balochi <alibalochi1910@gmail.com>
2026-06-23 19:12:29 +0330

    impliment line class

---
+++ classes/line.py
@@ -0,0 +1,8 @@
+class Line:
+    def __init__(self,name,origin,destination,station,station_count):
+        self.name = name
+        self.origin = origin
+        self.destination = destination
+        self.station = station
+        self.station_count = station_count
+        
