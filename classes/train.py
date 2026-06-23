09e10e5c3f4745f16dd9146e3d3173372bfb0c0d
amirhosseineilat <amirhosseineilat@gmail.com>
2026-06-23 23:26:17 +0330

    random id generator

--- classes/train.py
+++ classes/train.py
@@ -1,6 +1,14 @@
+import uuid 
+
+#here we use the uuid module that makes a random uuid (for example : hh46dghr67h-hdu7-7u47) and return the frist four character
+def get_id():
+    uid = uuid.uuid4()
+    return str(uid)[0:3]
+
+
 class Train:
     def __init__(self,name,line,avarage_speed,quality,ticket_cost,capacity):
-        self.id = 1
+        self.id = get_id()
         self.name = name
         self.line = line
         self.avarage_speed = avarage_speed
