c86b5809b6f570916975975a8c838477dae5afbd
amirhosseineilat <amirhosseineilat@gmail.com>
2026-06-24 00:07:30 +0330

    adding the str method for line

--- classes/line.py
+++ classes/line.py
@@ -5,4 +5,7 @@ class Line:
         self.destination = destination
         self.station = station
         self.station_count = station_count
+
+    def __str__(self):
+        return f"Name: {self.name}\nOrigin: {self.origin}\nDestination: {self.destination}\nStation: {self.station}\nStation_count: {self.station_count}\n"    
         
