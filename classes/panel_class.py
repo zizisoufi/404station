650ffebf40181205505a3ff97b62f0373f408c7b
Pooya Shourangiz <44345555+pooywa@users.noreply.github.com>
2026-06-25 18:36:35 +0330

    REFACTOR: move main panel logic to new main_panel.py file

--- classes/panel_class.py
+++ classes/panel_class.py
@@ -5,36 +5,6 @@ from classes.line import Line
 from classes.train import Train
 from utilitys import backButton
 
-class Panel:
-    def __init__(self):
-        self.db   = DataBase()
-        self.auth = Authentication(database=self.db)
-        #Default admin username and password
-        admin = Admin_User("admin", "admin")
-        # self.auth.rigester(admin)
-        self.db.create_DI(admin, "admins")
-
-    def start(self):
-        while True:
-            print("\nBe 404 Station Khosh Omadi")
-            print("1. Admin Panel")
-            print("2. Employer")
-            print("3. Passenger")
-            print("4. Exit")
-
-            choice = input("Mikhay Koja Beri? ")
-
-            if choice == "1":
-                self.admin_login_panel()
-            elif choice == "2":
-                self.employer_login_panel()
-            elif choice == "3":
-                self.passenger_panel()
-            elif choice == "4":
-                print("Shab O RoozegaR Khosh")
-                exit()
-            else:
-                print("Dari Eshatebah Mizani Dadash")
 
     # Admin
 
