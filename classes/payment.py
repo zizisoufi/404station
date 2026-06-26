89f39c8369e3131eaff986378a704af30835ffe3
Pooya Shourangiz <pooyashourangiz@gmail.com>
2026-06-26 14:42:30 +0330

    add method for choose card and new card

--- classes/payment.py
+++ classes/payment.py
@@ -1,7 +1,6 @@
 from classes.Card import Card
 from BANK import API
 from classes.user import Passenger
-from classes.Wallet import Wallet
 
 class PaymentService:
     def __init__(self) -> None:
@@ -20,8 +19,8 @@ class PaymentService:
             card_number = input("shomare cartet chande?").strip()
             exp_month = int(input("mahe enghezaye kartet chiye?").strip())
             exp_year = int(input("sale enghezaye kartet chiye?").strip())
-            password = int(input("password kartet chiye?").strip())
-            cvv2 = int(input("cvv2 enghezaye kartet chiye?").strip())
+            password = input("password kartet chiye?").strip()
+            cvv2 = input("cvv2 enghezaye kartet chiye?").strip()
             
             payment = self.bank.pay(card_number, exp_month, exp_year, password, cvv2, amount)
 
@@ -39,10 +38,41 @@ class PaymentService:
             return False 
         passenger.wallet -= amount
 
-
+    def _read_new_card(self):
+        try:
+            card_number = input("Card Number: ").strip()
+            exp_month = int(input("Card Number: ").strip())
+            exp_year = int(input("Card Number: ").strip())
+            password = input("Card Number: ").strip()
+            cvv2 = input("Card Number: ").strip()
+            
+            
+            return Card(card_number, exp_month, exp_year, password, cvv2)
+        except ValueError:
+            print("invalid card")
+            return None
+    
+    def _choose_saved_Card(self, passenger):
+        if not passenger.cards:
+            print("you dont have any card")
+            return None
+        self.show_my_card(passenger)
+        
+        try:
+            choice = int(input("choose card number: ").strip())
+            if 1 <= choice <= len(passenger.cards):
+                return passenger.cards[choice -1]
+        except ValueError:
+            pass
+        print("invalid card choice")
+        
+        
     def show_wallet_balance(self, passenger):
         print("\nWallet Balance")
         print(f"your walllet amount is {passenger.wallet}")
+    
+    def show_my_card(self, passenger):
+        return passenger.cards
         
     
          
